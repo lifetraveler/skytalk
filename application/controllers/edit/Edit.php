8bc26f2c4f9bc413ac3d127dbc95495712fd4834
lt_zhn <lifetraveler@outlook.com>
2017-02-03 10:08:04 +0800

    2017-1-24 zhangnan 正式部署

--- application/controllers/edit/Edit.php
+++ application/controllers/edit/Edit.php
@@ -23,15 +23,15 @@ class Edit extends CI_Controller{
 
     public function index()
     {
-//      echo 1;
+        echo 1;
 //      $this->load->view('lifetraveler/index');
 //        $data = $this->editdb->get_TargetArticle('1','');
         $data = $this->editdb->get_TargetArticle('1');
-        $this->output->set_content_type('application/json');
-        $this->output->set_header("Access-Control-Allow-Headers: Content-type");
-        $this->output->set_header("Access-Control-Allow-Origin", "*");
-        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
-        $this->output->set_output(json_encode($data));//->_display();
+//        $this->output->set_content_type('application/json');
+//        $this->output->set_header("Access-Control-Allow-Headers: Content-type");
+//        $this->output->set_header("Access-Control-Allow-Origin", "*");
+//        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+//        $this->output->set_output(json_encode($data));//->_display();
     }
 
     public function read($articleid,$userid){
