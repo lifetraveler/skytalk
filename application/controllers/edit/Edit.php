3b43b94623d172939e1dad1412432c7af7059c4b
lt_zhn <lifetraveler@outlook.com>
2017-02-03 10:48:40 +0800

    2017-2-3 zhangnan 跨域response 错误

--- application/controllers/edit/Edit.php
+++ application/controllers/edit/Edit.php
@@ -29,8 +29,8 @@ class Edit extends CI_Controller{
         $data = $this->editdb->get_TargetArticle('1');
         $this->output->set_content_type('application/json');
         $this->output->set_header("Access-Control-Allow-Headers: Content-type");
-        $this->output->set_header("Access-Control-Allow-Origin", "*");
-//        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+//        $this->output->set_header("Access-Control-Allow-Origin", "*");
+        $this->output->set_header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
         $this->output->set_output(json_encode($data));//->_display();
     }
 
